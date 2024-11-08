8b326473be866f59b140ed4a7eaeb35dfe71282d
Gabriel <gabertrol@gmail.com>
2024-11-08 16:12:16 -0300

    feat: add authentication with cleark

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,15 @@
-import { LogInIcon } from "lucide-react";
 import Image from "next/image";
-
 import { Button } from "../_components/ui/button";
+import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default async function LoginPage() {
+export default async function LoginPage () {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/");
+  }
   return (
     <div className="grid h-full grid-cols-2">
       {/* ESQUERDA */}
@@ -21,10 +27,12 @@ export default async function LoginPage() {
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-        <Button variant="outline">
-          <LogInIcon className="mr-2" />
-          Fazer login ou criar conta
-        </Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
       {/* DIREITA */}
       <div className="relative h-full w-full">
@@ -37,4 +45,4 @@ export default async function LoginPage() {
       </div>
     </div>
   );
-}
+};
