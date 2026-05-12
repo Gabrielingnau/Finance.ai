6c6c98aa4f6ff4349297391e29f15fa4d69525ac
Gabriel Lingnau <gabrielingnau@gmail.com>
2026-05-11 23:27:09 -0400

    teste

--- app/_actions/upsert-transaction/schema.ts
+++ app/_actions/upsert-transaction/schema.ts
@@ -1,9 +1,9 @@
+import { z } from "zod";
 import {
   TransactionCategory,
-  TransactionPaymentMethod,
   TransactionType,
-} from "@prisma/client";
-import { z } from "zod";
+} from "../../../generated/prisma";
+import { TransactionPaymentMethod } from "@prisma/client";
 
 export const upsertTransactionSchema = z.object({
   name: z.string().trim().min(1),
